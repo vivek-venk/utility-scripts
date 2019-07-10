b8605d01692bb309e63fa38144d59acdfa16c59a
vivek-venk <vivek.venk@gmail.com>
2019-07-10 15:28:42 +0530

    Update to file to add pid as a string variable

--- java_process.sh
+++ java_process.sh
@@ -2,12 +2,15 @@
 
 java_process=( proc1 proc2 proc3 proc4 proc5 proc5 )
 pid_array=()
+pidstr= ""
 for i in "${java_process[@]}"
 do
         echo "Java Process to search: " $i
         pid=`ps aux | grep java | grep -i $i | cut -d" " -f3`
         echo "Process Id: " $pid
         pid_array+=("$pid")
+        pidstr+=" "$pid
 done
 
 echo "Array PID: " ${pid_array[@]}
+echo "Pid String: " $pidstr
