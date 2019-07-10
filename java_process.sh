d8f501a0287470a17a7d7027dc24f050129ab156
vivek-venk <vivek.venk@gmail.com>
2019-07-10 15:18:40 +0530

    Script to get the java process in an array

---
+++ java_process.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+java_process=( proc1 proc2 proc3 proc4 proc5 proc5 )
+pid_array=()
+for i in "${java_process[@]}"
+do
+        echo "Java Process to search: " $i
+        pid=`ps aux | grep java | grep -i $i | cut -d" " -f3`
+        echo "Process Id: " $pid
+        pid_array+=("$pid")
+done
+
+echo "Array PID: " ${pid_array[@]}
