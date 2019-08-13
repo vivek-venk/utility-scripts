0633656a3b0d0d574c53bd78f5309a46c18dfc0f
vivek-venk <vivek.venk@gmail.com>
2019-08-13 11:19:43 +0530

    Script for monitoring a basic VM config

---
+++ monitor-script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+###################################################################
+# This is a comment in the file.
+# This script is an attempt to write a basic monitoring for a hostname.
+# Author: 		Vivek Venkatachalam
+# Date of Creation: 	12th August 2019
+# Comments to add more montiroing parameters are welcome.
+###################################################################
+
+echo "*****************************************"
+echo "Host Name: " `hostname`
+echo "*****************************************"
+echo "*****************************************"
+echo "Date:Time: " `date`
+echo "*****************************************"
+echo "Disk Space Usage"
+echo "*****************************************"
+df -h
+echo "*****************************************"
+echo "Disk Utilization - Top 10 Records"
+echo "*****************************************"
+du -h | sort -n -r | head -10
+echo "*****************************************"
